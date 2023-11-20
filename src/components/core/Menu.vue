22991b24703598e5017667a27b0a58c4445f111f
Salay1942 <l.bounlerth@gmail.com>
2023-11-20 16:07:11 +0700

    Menu

--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -14,48 +14,65 @@
         </div>
       </router-link>
 
-      <v-list nav dense>
-        <div v-for="(link, i) in links" :key="i">
-          <v-list-item v-if="!link.subLinks" :to="link.to" class="v-list-item">
-            <v-list-item-icon>
-              <v-icon>{{ link.icon }}</v-icon>
-            </v-list-item-icon>
+      <v-list>
+        <v-list-item
+          v-for="([title, icon, route], i) in menus"
+          :key="i"
+          link
+          @click="onClickMenu(route)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-title
-              style="margin-left: 6px; font-size: 15px; font-weight: 200"
-              >{{ link.text }}</v-list-item-title
-            >
-          </v-list-item>
+          <v-list-item-title>{{ title }}</v-list-item-title>
+        </v-list-item>
 
-          <v-list-group
-            v-else
-            :key="link.text"
-            no-action
-            :prepend-icon="link.icon"
-            :value="false"
-          >
+        <v-list-group
+          :value="false"
+          prepend-icon="mdi-account-circle"
+          v-for="([title], i) in admins"
+          :key="i"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>{{ title }}</v-list-item-title>
+          </template>
+          <!-- <v-list-group :value="false" no-action sub-group>
             <template v-slot:activator>
-              <v-list-item-title
-                style="margin-left: 6px; font-weight: 100; font-size: 15px"
-                >{{ link.text }}</v-list-item-title
-              >
+              <v-list-item-content>
+                <v-list-item-title>{{ title }}</v-list-item-title>
+              </v-list-item-content>
             </template>
 
-            <v-list-item
-              v-for="sublink in link.subLinks"
-              :to="sublink.to"
-              :key="sublink.text"
-            >
+            <v-list-item v-for="([title], i) in admins" :key="i" link>
+              <v-list-group :value="false" no-action sub-group>
+                <template v-slot:activator>
+                  <v-list-item-title>{{ title }} </v-list-item-title>
+                </template>
+                <v-list-item v-for="([title, icon], i) in cruds" :key="i" link>
+                  <v-list-item-title>{{ title }}</v-list-item-title>
+                  <v-list-item-icon>
+                    <v-icon>{{ icon }}</v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-group>
+            </v-list-item>
+          </v-list-group> -->
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Actions</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="([title, icon], i) in cruds" :key="i" link>
+              <v-list-item-title>{{ title }}</v-list-item-title>
               <v-list-item-icon>
-                <v-icon>{{ sublink.icon }}</v-icon>
+                <v-icon>{{ icon }}</v-icon>
               </v-list-item-icon>
-              <v-list-item-title
-                style="margin-left: 6px; font-size: 15px; padding: 8px"
-                >{{ sublink.text }}</v-list-item-title
-              >
             </v-list-item>
           </v-list-group>
-        </div>
+        </v-list-group>
       </v-list>
 
       <!-- <v-list shaped>
@@ -89,9 +106,9 @@ export default {
   name: "Menu",
   props: [],
   mounted() {
-    // this.selectedMenu = this.menus.findIndex(
-    //   (menu) => menu[2] == this.$route.path
-    // );
+    this.selectedMenu = this.menus.findIndex(
+      (menu) => menu[2] == this.$route.path
+    );
   },
   data() {
     return {
@@ -120,6 +137,17 @@ export default {
       //   ],
       // ],
 
+      menus: [["ໜັາຫລັກ", "mdi-home-outline", "/home"]],
+
+      admins: [["Management", "mdi-account-multiple-outline"]],
+
+      cruds: [
+        ["Create", "mdi-plus-outline"],
+        ["Read", "mdi-file-outline"],
+        ["Update", "mdi-update"],
+        ["Delete", "mdi-delete"],
+      ],
+
       links: [
         {
           to: "/dass",
@@ -138,11 +166,11 @@ export default {
               text: "ເເກ້ໄຂຂໍ້ມູນສິນຄ້າ",
               subLinks: [
                 {
-                  text: "111",
+                  text: "Add",
                   to: "/111",
                 },
                 {
-                  text: "222",
+                  text: "Edit",
                   to: "/222",
                 },
               ],
